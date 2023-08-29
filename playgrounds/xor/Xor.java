fe2ddf6f5a9ca32bd50879227629af4afa46a75e
Jackaleb <142457659+Jackaleb@users.noreply.github.com>
2023-08-28 17:02:31 -0700

    Updating playgrounds/xor/Xor.java

--- playgrounds/xor/Xor.java
+++ playgrounds/xor/Xor.java
@@ -29,7 +29,7 @@ public class Xor {
 
   public void decode(String text , int initKey){
 
-    for(int i = 0; i < text.length - 2; i++){
+    for(int i = 0; i < text.length() - 2; i++){
      byteHolder.add(Integer.parseInt(text.substring(i, i + 2),16));
     }
 
