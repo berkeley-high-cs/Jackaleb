ae453ced0f07ace65f67e5103279e8a44aa89bc4
Jackaleb <142457659+Jackaleb@users.noreply.github.com>
2023-12-17 05:27:07 +0000

    randomVariable changes

--- projects/monte-carlo/RandomVariable.java
+++ projects/monte-carlo/RandomVariable.java
@@ -1,3 +1,34 @@
-public class RandomVariable {
-    public double next();
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+import java.util.*;
+
+interface RandomVariable{
+    double next();
+}
+
+class NormalRandomVariable implements RandomVariable {
+
+    private Random r = new Random();
+    double [] data = {};
+    private double mean = Mean.mean(data);
+    private double stdDev = StandardDev.estimatedVarience(data);
+    private ArrayList<Double> additionalValues;
+
+    private ArrayList<Double> normalValues(int population){
+        for (int i = 0; i < population; i++) {
+            double randomNum = mean + stdDev r.nextGaussian();
+            additionalValues.add(randomNum);
+        }
+
+        Collections.sort(additionalValues);
+        return additionalValues;
+    }
+
+    @Override
+    public double next() {
+        // Provide a meaningful implementation for the next() method
+        return mean + stdDev * r.nextGaussian();
+    }
+
 }
